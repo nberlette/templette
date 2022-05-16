a3fff26f5444a050d803e0ce1d7ce3d841743d03
Nicholas Berlette <nick@berlette.com>
2022-05-15 17:07:16 -0700

    📦 1.0.0

--- unbuild.config.ts
+++ unbuild.config.ts
@@ -31,41 +31,5 @@ export default defineBuildConfig({
 		esbuild: {
 			banner: `/**\n * ${pkg.name}\n * @version {${pkg.version}}\n * @author {${pkg.author}}\n */\n`,
 		},
-	},
-	// hooks: {
-	// 	'rollup:options'(ctx, options) {
-	// 		// extract only the array type from options param. since it's external and non-importable,
-	// 		// the only alternative is to add rollup typedefs to devDependencies just for this one type.
-	// 		type RollupOutputOptions = Extract<typeof options.output, any[]>;
-	// 		type OriginalOutputOptions = typeof options.output;
-
-	// 		// coerce andmutate the build options object
-	// 		options.output = (
-	// 			[
-	// 				// first coerce options.out into its array form
-	// 				...(options.output as RollupOutputOptions),
-	// 				{
-	// 					file: './dist/index.umd.js',
-	// 					format: 'umd',
-	// 					generatedCode: {
-	// 						symbols: true,
-	// 						arrowFunctions: false,
-	// 						reservedNamesAsProps: false,
-	// 						objectShorthand: true
-	// 					}
-	// 				}
-	// 			] as RollupOutputOptions
-	// 		).flatMap((output) => {
-	// 			return ({
-	// 				...output,
-	// 				// conver .cjs -> .js
-	// 				...(['commonjs', 'cjs'].includes(output.format) ? { file: './dist/index.js' } : {}),
-	// 				// minify
-	// 				compact: true,
-	// 			} as typeof output)
-	// 		}) as OriginalOutputOptions;
-
-	// 		ctx.options.
-	// 	}
-	// }
+	}
 })
